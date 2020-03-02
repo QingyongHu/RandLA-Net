a6a04c5f04bebd3425f4074cb5f3f80cfe84a0c5
qingyong hu <qingyong.hu@cs.ox.ac.uk>
2020-03-02 20:58:25 +0000

    First commit!

--- jobs_6_fold_cv_s3dis.sh
+++ jobs_6_fold_cv_s3dis.sh
@@ -1,9 +1,9 @@
-#python -B main_S3DIS.py --gpu 0 --mode train --test_area 1
-#python -B main_S3DIS.py --gpu 0 --mode test --test_area 1
-#python -B main_S3DIS.py --gpu 0 --mode train --test_area 2
-#python -B main_S3DIS.py --gpu 0 --mode test --test_area 2
-#python -B main_S3DIS.py --gpu 0 --mode train --test_area 3
-#python -B main_S3DIS.py --gpu 0 --mode test --test_area 3
+python -B main_S3DIS.py --gpu 0 --mode train --test_area 1
+python -B main_S3DIS.py --gpu 0 --mode test --test_area 1
+python -B main_S3DIS.py --gpu 0 --mode train --test_area 2
+python -B main_S3DIS.py --gpu 0 --mode test --test_area 2
+python -B main_S3DIS.py --gpu 0 --mode train --test_area 3
+python -B main_S3DIS.py --gpu 0 --mode test --test_area 3
 python -B main_S3DIS.py --gpu 0 --mode train --test_area 4
 python -B main_S3DIS.py --gpu 0 --mode test --test_area 4
 python -B main_S3DIS.py --gpu 0 --mode train --test_area 5
