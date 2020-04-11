e406ba38b8f46cb8d015bbea906ebd8505ffbe33
qingyong hu <qingyong.hu@cs.ox.ac.uk>
2020-04-11 10:16:27 +0100

    Add visualization code

--- utils/6_fold_cv.py
+++ utils/6_fold_cv.py
@@ -5,6 +5,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(ROOT_DIR)
 from helper_ply import read_ply
+from helper_tool import Plot
 
 if __name__ == '__main__':
     base_dir = '/data/S3DIS/results'
@@ -17,6 +18,7 @@ if __name__ == '__main__':
     gt_classes = [0 for _ in range(13)]
     positive_classes = [0 for _ in range(13)]
     true_positive_classes = [0 for _ in range(13)]
+    visualization = False
 
     for file_name in data_path:
         pred_data = read_ply(file_name)
@@ -24,6 +26,16 @@ if __name__ == '__main__':
         original_data = read_ply(os.path.join(original_data_dir, file_name.split('/')[-1][:-4] + '.ply'))
         labels = original_data['class']
         points = np.vstack((original_data['x'], original_data['y'], original_data['z'])).T
+
+        ##########################################################################################
+        # Visualize data #
+        ##########################################################################################
+        if visualization:
+            colors = np.vstack((original_data['red'], original_data['green'], original_data['blue'])).T
+            xyzrgb = np.concatenate([points, colors], axis=-1)
+            Plot.draw_pc(xyzrgb)  # visualize raw point clouds
+            Plot.draw_pc_sem_ins(points, labels)  # visualize ground-truth
+            Plot.draw_pc_sem_ins(points, pred)  # visualize prediction
 
         correct = np.sum(pred == labels)
         print(str(file_name.split('/')[-1][:-4]) + '_acc:' + str(correct / float(len(labels))))
