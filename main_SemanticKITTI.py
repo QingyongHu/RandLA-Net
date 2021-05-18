d74af96c93e63a9f6184ec5acc6331ea43778c99
Arkcia <Arkcia@126.com>
2021-05-18 12:11:39 +0800

    grid_size fix

--- main_SemanticKITTI.py
+++ main_SemanticKITTI.py
@@ -12,7 +12,7 @@ import os, argparse, pickle
 class SemanticKITTI:
     def __init__(self, test_id):
         self.name = 'SemanticKITTI'
-        self.dataset_path = '/data/semantic_kitti/dataset/sequences_0.06'
+        self.dataset_path = '/data/semantic_kitti/dataset/sequences_0.12'
         self.label_to_names = {0: 'unlabeled',
                                1: '1 person',
                                2: '2+ person',
