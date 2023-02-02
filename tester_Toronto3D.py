36cf67507b884e824fb2cb6e8168047ff38c734c
WeikaiTan <WeikaiTan@users.noreply.github.com>
2023-02-02 18:50:12 -0500

    added prediction evaluation

--- tester_Toronto3D.py
+++ tester_Toronto3D.py
@@ -6,6 +6,7 @@ from helper_ply import read_ply, write_ply
 import tensorflow as tf
 import numpy as np
 import time
+from sklearn.metrics import confusion_matrix
 
 
 def log_string(out_str, log_out):
@@ -15,7 +16,7 @@ def log_string(out_str, log_out):
 
 
 class ModelTester:
-    def __init__(self, model, dataset, restore_snap=None):
+    def __init__(self, model, dataset, config, restore_snap=None):
         # Tensorflow Saver definition
         my_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
         self.saver = tf.train.Saver(my_vars, max_to_keep=100)
@@ -39,9 +40,10 @@ class ModelTester:
         self.test_probs = [np.zeros((l.data.shape[0], model.config.num_classes), dtype=np.float16)
                            for l in dataset.input_trees['test']]
 
+        self.config = config
         self.log_out = open('log_test_' + dataset.name + '.txt', 'a')
 
-    def test(self, model, dataset, num_votes=20):
+    def test(self, model, dataset, num_votes=20, eval=False):
 
         # Smoothing parameter for votes
         test_smooth = 0.98
@@ -107,7 +109,10 @@ class ModelTester:
                     i_test = 0
                     for i, file_path in enumerate(files):
                         # Get file
-                        points = self.load_evaluation_points(file_path)
+                        if eval:
+                            points, gt = self.load_evaluation_points(file_path)
+                        else:
+                            points = self.load_test_points(file_path)
 
                         # Reproject probs
                         probs = np.zeros(shape=[np.shape(points)[0], 8], dtype=np.float16)
@@ -130,6 +135,10 @@ class ModelTester:
                         write_ply(ply_name, [points, preds], ['x', 'y', 'z', 'preds'])
                         log_string(ply_name + ' has saved', self.log_out)
 
+                        # evaluate prediction results
+                        if eval:
+                            self.evaluate(preds, gt)
+
                         i_test += 1
 
                     t2 = time.time()
@@ -144,6 +153,60 @@ class ModelTester:
         return
 
     @staticmethod
-    def load_evaluation_points(file_path):
+    def load_test_points(file_path):
         data = read_ply(file_path)
         return np.vstack((data['x'], data['y'], data['z'])).T
+    
+    @staticmethod
+    def load_evaluation_points(file_path):
+        data = read_ply(file_path)
+        xyz = np.vstack((data['x'], data['y'], data['z'])).T
+        label = data['scalar_Label'].astype(np.uint8)
+        return xyz, label
+    
+    def evaluate(self, pred, gt):
+        gt_classes = [0 for _ in range(self.config.num_classes)]
+        positive_classes = [0 for _ in range(self.config.num_classes)]
+        true_positive_classes = [0 for _ in range(self.config.num_classes)]
+        val_total_correct = 0
+        val_total_seen = 0
+
+        if not self.config.ignored_label_inds:
+            pred_valid = pred
+            labels_valid = gt
+        else:
+            invalid_idx = np.where(gt == self.config.ignored_label_inds)[0]
+            labels_valid = np.delete(gt, invalid_idx)
+            labels_valid = labels_valid - 1
+            pred_valid = np.delete(pred, invalid_idx)
+            pred_valid = pred_valid - 1
+
+        correct = np.sum(pred_valid == labels_valid)
+        val_total_correct += correct
+        val_total_seen += len(labels_valid)
+
+        conf_matrix = confusion_matrix(labels_valid, pred_valid, np.arange(0, self.config.num_classes, 1))
+        gt_classes += np.sum(conf_matrix, axis=1)
+        positive_classes += np.sum(conf_matrix, axis=0)
+        true_positive_classes += np.diagonal(conf_matrix)
+
+        iou_list = []
+        for n in range(0, self.config.num_classes, 1):
+            iou = true_positive_classes[n] / float(gt_classes[n] + positive_classes[n] - true_positive_classes[n])
+            iou_list.append(iou)
+        mean_iou = sum(iou_list) / float(self.config.num_classes)
+
+        log_string('eval accuracy: {}'.format(val_total_correct / float(val_total_seen)), self.log_out)
+        log_string('mean IOU:{}'.format(mean_iou), self.log_out)
+
+        mean_iou = 100 * mean_iou
+        log_string('Mean IoU = {:.1f}%'.format(mean_iou), self.log_out)
+        s = '{:5.2f} | '.format(mean_iou)
+        for IoU in iou_list:
+            s += '{:5.2f} '.format(100 * IoU)
+        log_string('-' * len(s), self.log_out)
+        log_string(s, self.log_out)
+        log_string('-' * len(s) + '\n', self.log_out)
+        return mean_iou
+
+
