ed0387c93a32293daeb671255f6f8657d18c4428
Anass Yarroudh <72500344+Yarroudh@users.noreply.github.com>
2023-07-12 00:30:42 +0200

    Update data_prepare_semantickitti.py

--- utils/data_prepare_semantickitti.py
+++ utils/data_prepare_semantickitti.py
@@ -17,8 +17,8 @@ remap_lut = np.zeros((max_key + 100), dtype=np.int32)
 remap_lut[list(remap_dict.keys())] = list(remap_dict.values())
 
 grid_size = 0.06
-dataset_path = '/data/semantic_kitti/dataset/sequences'
-output_path = '/data/semantic_kitti/dataset/sequences' + '_' + str(grid_size)
+dataset_path = 'data/semantic_kitti/dataset/sequences'
+output_path = 'data/semantic_kitti/dataset/sequences' + '_' + str(grid_size)
 seq_list = np.sort(os.listdir(dataset_path))
 
 for seq_id in seq_list:
